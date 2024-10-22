a0073fbc167f9e673d024dcaabe962747975a647
RonCarreon30 <ronniecarreon30@gmail.com>
2024-10-22 11:53:43 +0800

    Update reservadb.sql

--- database/reservadb.sql
+++ database/reservadb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2024 at 01:48 AM
+-- Generation Time: Oct 22, 2024 at 05:52 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,23 +36,26 @@ CREATE TABLE `assigned_rooms_tbl` (
   `end_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `assigned_rooms_tbl`
+-- Table structure for table `buildings_tbl`
 --
 
-INSERT INTO `assigned_rooms_tbl` (`assignment_id`, `schedule_id`, `room_id`, `day`, `start_time`, `end_time`) VALUES
-(243, 427, 9, 'Monday', '09:00:00', '10:30:00'),
-(244, 428, 5, 'Wednesday', '09:00:00', '10:30:00'),
-(245, 429, 5, 'Tuesday', '11:00:00', '12:30:00'),
-(246, 430, 5, 'Thursday', '11:00:00', '12:30:00'),
-(247, 431, 7, 'Monday', '14:00:00', '15:30:00'),
-(248, 432, 7, 'Thursday', '14:00:00', '15:30:00'),
-(249, 433, 4, 'Monday', '09:00:00', '10:30:00'),
-(250, 434, 6, 'Wednesday', '09:00:00', '10:30:00'),
-(251, 435, 6, 'Tuesday', '11:00:00', '12:30:00'),
-(252, 436, 6, 'Thursday', '11:00:00', '12:30:00'),
-(253, 437, 7, 'Monday', '14:00:00', '15:30:00'),
-(254, 438, 7, 'Thursday', '14:00:00', '15:30:00');
+CREATE TABLE `buildings_tbl` (
+  `building_id` int(11) NOT NULL,
+  `building_name` varchar(100) NOT NULL,
+  `building_desc` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buildings_tbl`
+--
+
+INSERT INTO `buildings_tbl` (`building_id`, `building_name`, `building_desc`, `created_at`) VALUES
+(2, 'Sample', 'Sample', '2024-10-22 01:30:59'),
+(3, 'CEIT', 'Civil Engineering and Information Technology Building', '2024-10-22 01:45:51');
 
 -- --------------------------------------------------------
 
@@ -63,20 +66,15 @@ INSERT INTO `assigned_rooms_tbl` (`assignment_id`, `schedule_id`, `room_id`, `da
 CREATE TABLE `dept_tbl` (
   `dept_id` int(11) NOT NULL,
   `dept_name` varchar(100) NOT NULL,
-  `dept_building` varchar(100) NOT NULL
+  `building_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `dept_tbl`
 --
 
-INSERT INTO `dept_tbl` (`dept_id`, `dept_name`, `dept_building`) VALUES
-(1, 'Information Technology', 'CEIT'),
-(2, 'Civil Engineering', 'CEIT'),
-(3, 'Electrical Engineering', 'CEIT'),
-(4, 'Business Administration', 'CABA'),
-(5, 'Education', 'COED'),
-(8, 'Accountancy', 'CABA');
+INSERT INTO `dept_tbl` (`dept_id`, `dept_name`, `building_id`) VALUES
+(1, 'Information Technology', 3);
 
 -- --------------------------------------------------------
 
@@ -103,8 +101,7 @@ INSERT INTO `facilities` (`id`, `facility_name`, `building`, `status`, `descri`)
 (19, 'Collab 1', 'CEIT', 'Available', 'Located at the 3rd floor of CEIT\r\n'),
 (20, 'Multi-purpose Hall', 'CEIT', 'Available', 'MPH is located at the 6th floor of CEIT Building'),
 (21, 'Collab 2', 'CEIT', 'Available', 'CEIT Collab room at 3rd Floor'),
-(22, 'Collab 3', 'CEIT', 'Available', 'Located at 3rd foor of CEIT building'),
-(23, 'Sample', 'SC/MAIN', 'Available', 'Sample');
+(22, 'Collab 3', 'CEIT', 'Available', 'Located at 3rd foor of CEIT building');
 
 -- --------------------------------------------------------
 
@@ -138,8 +135,7 @@ CREATE TABLE `reservations` (
 INSERT INTO `reservations` (`id`, `user_id`, `user_department`, `facility_id`, `facility_name`, `start_date`, `end_date`, `reservation_date`, `start_time`, `end_time`, `additional_info`, `facultyInCharge`, `purpose`, `reservation_status`, `rejection_reason`, `created_at`) VALUES
 (17, 18, 'Civil Engineering', 20, 'Multi-purpose Hall', NULL, NULL, '2024-08-31', '07:00:00', '09:00:00', 'CEIT Event', '', 'Symposium', 'Expired', '', '2024-05-29 01:23:19'),
 (18, 18, 'Civil Engineering', 21, 'Collab 2', NULL, NULL, '2024-08-31', '07:00:00', '10:00:00', 'IT Defense', 'Update', 'Defense', 'Expired', '', '2024-05-29 02:09:48'),
-(19, 18, 'Civil Engineering', 20, 'Multi-purpose Hall', NULL, NULL, '2024-08-31', '09:30:00', '12:00:00', 'CEIT Event', '', 'Symposium', 'Expired', '', '2024-05-29 01:23:19'),
-(34, 18, 'Civil Engineering', 10, 'Auditorium', NULL, NULL, '2024-10-16', '07:00:00', '10:00:00', 'CEIT Event\n', 'Einstein', 'Symposium', 'In Review', '', '2024-10-07 13:05:04');
+(34, 18, 'Civil Engineering', 10, 'Auditorium', NULL, NULL, '2024-10-16', '07:00:00', '10:00:00', 'CEIT Event\n', 'Einstein', 'Symposium', 'Expired', '', '2024-10-07 13:05:04');
 
 -- --------------------------------------------------------
 
@@ -196,24 +192,6 @@ CREATE TABLE `schedules_tbl` (
   `pref_dept` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `schedules_tbl`
---
-
-INSERT INTO `schedules_tbl` (`schedule_id`, `subject_code`, `subject`, `section`, `instructor`, `start_time`, `end_time`, `days`, `type`, `term_id`, `schedule_status`, `user_dept`, `pref_dept`) VALUES
-(427, 'MATH101', 'Calculus I', 'BSCE 1-1', 'Teacher A', '09:00:00', '10:30:00', 'Monday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(428, 'MATH101', 'Calculus I', 'BSCE 1-1', 'Teacher A', '09:00:00', '10:30:00', 'Wednesday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(429, 'ENG202', 'English Literature', 'BSCE 1-1', 'Teacher A', '11:00:00', '12:30:00', 'Tuesday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(430, 'ENG202', 'English Literature', 'BSCE 1-1', 'Teacher A', '11:00:00', '12:30:00', 'Thursday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(431, 'SCI303', 'Physics', 'BSCE 1-2', 'Teacher B', '14:00:00', '15:30:00', 'Monday', 'Laboratory', 1, 'Scheduled', 'Civil Engineering', 2),
-(432, 'SCI303', 'Physics', 'BSCE 1-2', 'Teacher B', '14:00:00', '15:30:00', 'Thursday', 'Laboratory', 1, 'Scheduled', 'Civil Engineering', 2),
-(433, 'MATH101', 'Calculus I', 'BSCE 1-1', 'Dr. Alan Turing', '09:00:00', '10:30:00', 'Monday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(434, 'MATH101', 'Calculus I', 'BSCE 1-1', 'Dr. Alan Turing', '09:00:00', '10:30:00', 'Wednesday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(435, 'ENG202', 'English Literature', 'BSCE 1-1', 'Prof. Jane Austen', '11:00:00', '12:30:00', 'Tuesday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(436, 'ENG202', 'English Literature', 'BSCE 1-1', 'Prof. Jane Austen', '11:00:00', '12:30:00', 'Thursday', 'Lecture', 1, 'Scheduled', 'Civil Engineering', 2),
-(437, 'SCI303', 'Physics', 'BSCE 1-2', 'Dr. Albert Einstein', '14:00:00', '15:30:00', 'Monday', 'Laboratory', 1, 'Scheduled', 'Civil Engineering', 2),
-(438, 'SCI303', 'Physics', 'BSCE 1-2', 'Dr. Albert Einstein', '14:00:00', '15:30:00', 'Thursday', 'Laboratory', 1, 'Scheduled', 'Civil Engineering', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -222,9 +200,9 @@ INSERT INTO `schedules_tbl` (`schedule_id`, `subject_code`, `subject`, `section`
 
 CREATE TABLE `terms_tbl` (
   `term_id` int(11) NOT NULL,
-  `academic_year` varchar(50) DEFAULT NULL,
-  `semester` varchar(50) DEFAULT NULL,
-  `term_status` varchar(20) NOT NULL DEFAULT 'Current'
+  `academic_year` varchar(20) NOT NULL,
+  `semester` varchar(20) NOT NULL,
+  `term_status` enum('Current','Upcoming','Expired') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -232,7 +210,8 @@ CREATE TABLE `terms_tbl` (
 --
 
 INSERT INTO `terms_tbl` (`term_id`, `academic_year`, `semester`, `term_status`) VALUES
-(1, '2024-2025', '1st Semester', 'Current');
+(1, '2024-2025', '1st Semester', 'Current'),
+(2, '2024-2025', '2nd Semester', 'Upcoming');
 
 -- --------------------------------------------------------
 
@@ -261,7 +240,10 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `contact_number`,
 (18, 'Jane', 'Doe', 'stud@email.com', '0123456789', 'Civil Engineering', 'Student Rep', '$2y$10$gvSMe1SalqUDTnv6Zvx2NeV5qS6VTL1ZBEyqCvOatRsp.SykkVduW'),
 (19, 'Aida', 'Bugg', 'gso@email.com', '0123456789', 'N/A', 'Facility Head', '$2y$10$.El9UgRRXVZBnpc9InQ00.DwORuCAFraNYSebnJING8iLFc7YQ8dy'),
 (20, 'Agape', 'Balbon', 'ageypp@email.com', '0123012', 'N/A', 'Registrar', '$2y$10$JqXEgNNMWCMeBDYfcEO7juFZw7FcN3paoi8se/dgoZaspa.T6Dfce'),
-(25, 'Juan', 'Dela Cruz', 'jdc@email.com', '0123456789', 'Information Technology', 'Facility Head', '$2y$10$kw93M5x0kqyhIQxERnfCrujv1JTnEJkuP/EHHseDLz5CSfom.Y52u');
+(28, 'IT', 'Student', 'student.IT@email.com', '0123456789', 'Information Technology', 'Student Rep', '$2y$10$jzZ1B3LgQvmWAUbnYBBuc.0Lw9JL8WWujtw9N1TFlFXvKMUVBjoge'),
+(31, 'IT', 'Head', 'head.IT@email.com', '0123456789', 'Information Technology', 'Dept. Head', '$2y$10$e7VAarZpK.kDwkumj8WYo.AC/V4leMSXeTYre2G3ve2rqX2XgG1HW'),
+(32, 'CE', 'Student', 'student.CE@email.com', '0123456789', 'Civil Engineering', 'Student Rep', '$2y$10$bhKuejIAfLXySXeIYyXiYuxHKtj.Ptl8ZXM0a5jqGdPQ.sprm0Fx2'),
+(34, 'CE', 'Head', 'head.CE@email.com', '012345789', 'Civil Engineering', 'Dept. Head', '$2y$10$/r0NrJd8FcEvrpnLeM0YuumkSXEGkwa1QyRoaZ07NWrvsfp3S9YjS');
 
 --
 -- Indexes for dumped tables
@@ -276,10 +258,17 @@ ALTER TABLE `assigned_rooms_tbl`
   ADD KEY `fk_room` (`room_id`);
 
 --
+-- Indexes for table `buildings_tbl`
+--
+ALTER TABLE `buildings_tbl`
+  ADD PRIMARY KEY (`building_id`);
+
+--
 -- Indexes for table `dept_tbl`
 --
 ALTER TABLE `dept_tbl`
-  ADD PRIMARY KEY (`dept_id`);
+  ADD PRIMARY KEY (`dept_id`),
+  ADD KEY `fk_building` (`building_id`);
 
 --
 -- Indexes for table `facilities`
@@ -330,7 +319,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assigned_rooms_tbl`
 --
 ALTER TABLE `assigned_rooms_tbl`
-  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=255;
+  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=279;
+
+--
+-- AUTO_INCREMENT for table `buildings_tbl`
+--
+ALTER TABLE `buildings_tbl`
+  MODIFY `building_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `dept_tbl`
+--
+ALTER TABLE `dept_tbl`
+  MODIFY `dept_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `facilities`
@@ -354,19 +355,19 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `schedules_tbl`
 --
 ALTER TABLE `schedules_tbl`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=439;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=487;
 
 --
 -- AUTO_INCREMENT for table `terms_tbl`
 --
 ALTER TABLE `terms_tbl`
-  MODIFY `term_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `term_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Constraints for dumped tables
@@ -378,6 +379,12 @@ ALTER TABLE `users`
 ALTER TABLE `assigned_rooms_tbl`
   ADD CONSTRAINT `fk_room` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`),
   ADD CONSTRAINT `fk_schedule` FOREIGN KEY (`schedule_id`) REFERENCES `schedules_tbl` (`schedule_id`);
+
+--
+-- Constraints for table `dept_tbl`
+--
+ALTER TABLE `dept_tbl`
+  ADD CONSTRAINT `fk_building` FOREIGN KEY (`building_id`) REFERENCES `buildings_tbl` (`building_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reservations`
@@ -397,7 +404,7 @@ DELIMITER $$
 --
 -- Events
 --
-CREATE DEFINER=`root`@`localhost` EVENT `update_reservation_status` ON SCHEDULE EVERY 1 HOUR STARTS '2024-10-07 19:58:42' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE reservations
+CREATE DEFINER=`root`@`localhost` EVENT `update_reservation_status` ON SCHEDULE EVERY 1 MINUTE STARTS '2024-10-07 19:58:42' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE reservations
     SET reservation_status = 'Expired' 
     WHERE reservation_date < CURDATE() 
       AND reservation_status <> 'Expired'$$
